aa6e02794e214d657cdc194f64c03731e634ba8f
Nafiseh Zeinali <n.zeinali@gmail.com>
2026-04-02 12:16:54 +0200

    Set Events page as home route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::inertia('/', 'Events/Index')->name('home');
-Route::get('/events', function () {
-    return Inertia::render('Events/Index');
-});
